3dc80f3e8b649f7e0acbd4001b9bd939cb5a49b6
3edsem <bossisthere15@gmail.com>
2025-12-07 02:32:08 +0530

    updated code and fix the re render

--- app.py
+++ app.py
@@ -124,7 +124,7 @@ def create_app():
             print(f"⚠️ Warning: Could not load Fraud Model: {e}")
 
     return app
+app = create_app()
 
 if __name__ == '__main__':
-    app = create_app()
     app.run(debug=True, host='0.0.0.0', port=5000)
