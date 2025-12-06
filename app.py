2da2c4e0c0450a03a165bfb24f77315ae0332a7e
3edsem <bossisthere15@gmail.com>
2025-12-07 04:47:34 +0530

    added netlify to cors origins

--- app.py
+++ app.py
@@ -28,7 +28,8 @@ def create_app():
     allowed_origins = [
         "http://localhost:3000",
         "http://127.0.0.1:3000",
-        "http://192.168.0.103:3000"
+        "http://192.168.0.103:3000",
+        "https://fraud-guard.netlify.app"
     ]
 
     # Add Production URL
