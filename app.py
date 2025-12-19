73247e7de6c08af4a8301941dde9aa8671813a70
3edsem <bossisthere15@gmail.com>
2025-12-19 20:45:39 +0530

    Revert "Auth route fix"
    This reverts commit 42014aba58a142f79fdb5046f81899d5f7b2e16e.

--- app.py
+++ app.py
@@ -1,9 +1,4 @@
 import os
-from dotenv import load_dotenv
-
-# Load environment variables from .env file
-load_dotenv()
-
 from flask import Flask, jsonify
 from flask_cors import CORS
 from config import Config
